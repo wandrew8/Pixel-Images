3ba4f89116054ecb583f1310e37ccf26d5fd3d76
Andrew Weiss <wandrew8@gmail.com>
2020-03-26 15:19:24 -0700

    cloudinary added to form

--- src/components/HomeHeader/HomeHeader.js
+++ src/components/HomeHeader/HomeHeader.js
@@ -23,9 +23,7 @@ export default class HomeHeader extends Component {
                     <a href="/"><h1>Search Stock Photos</h1></a>
                     <div className="tools">
                         <div onClick={this.showModal} className="searchButton"><i className="fas fa-search"></i>Search</div>
-                        <div onClick={this.showModal} className="formButton"><i className="far fa-images"></i>Sign Up</div>
-                        <div onClick={this.showModal} className="formButton"><i className="far fa-images"></i>Log In</div>
-
+                        <div className="formButton"><button><a href="/signup"><i className="fas fa-user"></i>Join</a></button></div>
                     </div>
                 </header>
                 <Modal show={this.state.show} handleClose={this.hideModal} />
@@ -35,7 +33,7 @@ export default class HomeHeader extends Component {
 }
 
 const Modal = ({ handleClose, show }) => {
-    const showHideClassName = show ? 'searchbar' : 'searchbar hidden';
+    const showHideClassName = show ? 'searchbar' : 'searchbar hideSearchBar';
   
     return (
       <div className={showHideClassName}>
