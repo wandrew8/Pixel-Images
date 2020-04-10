2f8154306fbc8739a80353dc54f56b58f2e13edc
Andrew Weiss <wandrew8@gmail.com>
2020-04-09 18:04:27 -0700

    logic to update comment likes and dislikes added

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -3,6 +3,58 @@ import moment from 'moment';
 import './Comment.scss';
 
 class Comment extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            likes: this.props.comment.likes,
+            dislikes: this.props.comment.dislikes,
+            liked: false,
+            disliked: false,
+
+        }
+    }
+
+    handleLike = () => {
+        this.setState({ liked: !this.state.liked})
+        if(!this.state.liked) {
+            this.incrementLikes();
+        } else {
+            this.decrementLikes();
+        }
+    }
+
+    handleDislike = () => {
+        this.setState({ disliked: !this.state.disliked})
+        if(!this.state.disliked) {
+            this.incrementDislikes();
+        } else {
+            this.decrementDislikes();
+        }
+    }
+
+    incrementLikes = () => {
+        console.log('You liked this comment')
+        this.setState({ likes: this.state.likes + 1 })
+    }
+
+    decrementLikes = () => {
+        console.log('You unliked this comment')
+        this.setState({ likes: this.state.likes - 1 })
+
+    }
+
+    incrementDislikes = () => {
+        console.log('You disliked this comment')
+        this.setState({ dislikes: this.state.dislikes + 1 })
+
+    }
+
+    decrementDislikes = () => {
+        console.log('You retracted your dislike')
+        this.setState({ dislikes: this.state.dislikes - 1 })
+
+    }
+
     render() {
         const { comment } = this.props;
         return (
@@ -14,8 +66,8 @@ class Comment extends Component {
                     <small>{`${comment.author.firstName} ${comment.author.lastName} | ${moment(new Date(comment.createdAt), "YYYYMMDD").fromNow()}`}</small>
                     <p>{comment.text}</p>
                     <div className="statusBar">
-                            <i className="far fa-thumbs-up"></i><p>{comment.likes}</p>
-                            <i className="far fa-thumbs-down"></i><p>{comment.dislikes}</p>
+                            <i onClick={this.handleLike} className="far fa-thumbs-up"></i><p>{this.state.likes}</p>
+                            <i onClick={this.handleDislike} className="far fa-thumbs-down"></i><p>{this.state.dislikes}</p>
                         <small>Reply</small>
                     </div>
                 </div>
--- src/components/CommentsBar/CommentsBar.js
+++ src/components/CommentsBar/CommentsBar.js
@@ -13,6 +13,7 @@ class CommentsBar extends React.Component {
             commentsData: [],
             comment: '',
             error: '',
+            
 
         }
         
