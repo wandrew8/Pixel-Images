4dff08017d08045214b01ca6a651f353dd67dc5b
Andrew Weiss <wandrew8@gmail.com>
2020-04-22 18:04:29 -0700

    Updates

---
+++ precache-manifest.c80116cdb25bb70882cc0ec450970d27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7aea9e5f9d5b8da1c4b02486065626",
+    "url": "/pixel-images/index.html"
+  },
+  {
+    "revision": "4f86d26a4e76475055c2",
+    "url": "/pixel-images/static/css/main.8d59412d.chunk.css"
+  },
+  {
+    "revision": "d08391269dffe2699e97",
+    "url": "/pixel-images/static/js/2.9f3133ad.chunk.js"
+  },
+  {
+    "revision": "fa6965d26a9a6baf24f856735056b065",
+    "url": "/pixel-images/static/js/2.9f3133ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f86d26a4e76475055c2",
+    "url": "/pixel-images/static/js/main.2de9b209.chunk.js"
+  },
+  {
+    "revision": "fc105a83fbf66797c418",
+    "url": "/pixel-images/static/js/runtime-main.9205d084.js"
+  },
+  {
+    "revision": "ed59d81f895a4718bb87b206a81caf69",
+    "url": "/pixel-images/static/media/android-chrome-512x512.ed59d81f.png"
+  }
+]);
