e807ea37cabc7acfda7a9a3b2a1ccab2e5ef9aeb
Andrew Weiss <wandrew8@gmail.com>
2020-04-23 21:33:06 -0700

    resolves styling issues

--- src/components/PhotoStats/PhotoStats.js
+++ src/components/PhotoStats/PhotoStats.js
@@ -4,17 +4,18 @@ import './PhotoStats.scss';
 
 class PhotoStats extends Component {
     render() {
-        const {likes, comments, createdAt, description, _id} = this.props.photo;
+        const { comments, likes, _id} = this.props.photo;
 
         return (
             <React.Fragment>
                 <Link className="viewLink" to={`/photo/${_id}`}>View</Link>
                 <div id="stats">
-                    <div className="stats">
-                        <div className="likes"><p>Comments: <em>{comments.length}</em><i className="far fa-comments"></i></p></div>
-                        <div className="likes"><p>Likes: <em>{this.props.photo.likes}</em><i className="far fa-heart"></i></p></div>
+                    <div className="statText">
+                        <div className="stats">
+                            <div className="likes"><p>Comments: <em>{comments.length}</em><i className="far fa-comments"></i></p></div>
+                            <div className="likes"><p>Likes: <em>{likes}</em><i className="far fa-heart"></i></p></div>
+                        </div>
                     </div>
-                    <p id="statDescription">{description}</p>
                 </div>
             </React.Fragment>
         )
