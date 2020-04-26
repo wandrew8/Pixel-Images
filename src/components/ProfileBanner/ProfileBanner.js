410e6a798a6f02d600301d31b6fe99ff0df0fd76
Andrew Weiss <wandrew8@gmail.com>
2020-04-25 17:35:34 -0700

    edit profile form added and functioning

--- src/components/ProfileBanner/ProfileBanner.js
+++ src/components/ProfileBanner/ProfileBanner.js
@@ -1,23 +1,94 @@
 import React from 'react';
 import moment from 'moment';
 import './ProfileBanner.scss';
+import UpdateProfileForm from '../UpdataProfileForm/UpdateProfileForm';
 
 
 class UserBanner extends React.Component {
-    render() {
-        const { firstName, lastName, userImage, createdAt } = this.props.author;
+    constructor(props) {
+        super(props);
+        this.state = {
+            showForm: false,
+            updatedInfo: false,
+            firstName: '',
+            lastName: '',
+            bio: '',
+            userImage: '',
+        }
+    }
+
+    showForm = () => {
+        this.setState({ showForm: true });
+    }
+
+    closeForm = () => {
+        this.setState({ showForm: false });
+    }
+
+    updateAuthorInfo = (data) => {
+        this.setState({
+            updatedInfo: true,
+            firstName: data.firstName,
+            lastName: data.lastName,
+            bio: data.bio,
+            userImage: data.userImage,
+        })
+    }
+
+    renderForm = () => {
         return (
-            <div className="banner">
-                <div className="editButton">Edit Profile<i className="fas fa-pencil-alt"></i></div>
-                <div className="userBanner">
-                    <img src={userImage} alt={`${firstName}`} />
-                    <div className="authorName">
-                        <h2>{firstName} {lastName}</h2>
-                        <p>Joined: {moment(new Date(createdAt)).format("MMMM Do, YYYY")}</p>
-                    </div>
-                </div>
+            <div className="formContainer">
+                <UpdateProfileForm updateAuthorInfo={this.updateAuthorInfo} closeForm={this.closeForm} author={this.props.author} />
             </div>
         )
+    }
+    render() {
+        const RenderBio = (props) => {
+            return (
+                <div className="bioContainer">
+                    <h2 className="biography">Biography</h2>
+                    <p>{props.bio}</p>
+                </div>
+            )
+        }
+        if(this.state.updatedInfo) {
+            const { firstName, lastName, userImage, bio } = this.state;
+            const { createdAt } = this.props.author; 
+            return (
+                <div className="banner">
+                    <div onClick={this.showForm}className="editButton">Edit Profile<i className="fas fa-pencil-alt"></i></div>
+                    <div className="bannerGrid">
+                        <div className="userBanner">
+                            <img src={userImage} alt={`${firstName}`} />
+                            <div className="authorName">
+                                <h2>{firstName} {lastName}</h2>
+                                <p>Joined: {moment(new Date(createdAt)).format("MMMM Do, YYYY")}</p>
+                            </div>
+                        </div>
+                        {bio ? <RenderBio bio={bio}/> : null}
+                    </div>
+                    {this.state.showForm ? this.renderForm() : null}
+                </div>
+            )
+        } else {
+            const { firstName, lastName, userImage, createdAt, bio } = this.props.author;
+            return (
+                <div className="banner">
+                    <div onClick={this.showForm}className="editButton">Edit Profile<i className="fas fa-pencil-alt"></i></div>
+                    <div className="bannerGrid">
+                        <div className="userBanner">
+                            <img src={userImage} alt={`${firstName}`} />
+                            <div className="authorName">
+                                <h2>{firstName} {lastName}</h2>
+                                <p>Joined: {moment(new Date(createdAt)).format("MMMM Do, YYYY")}</p>
+                            </div>
+                        </div>
+                        {bio && bio.length > 0 ? <RenderBio bio={bio} /> : null}
+                    </div>
+                    {this.state.showForm ? this.renderForm() : null}
+                </div>
+            )
+        }
     }
 }
 
---
+++ src/components/UpdataProfileForm/UpdateProfileForm.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import './UpdateProfileForm.scss';
+
+class UpdateProfileForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: this.props.author.firstName,
+            lastName: this.props.author.lastName,
+            bio: this.props.author.bio,
+            userImage: this.props.author.userImage,
+            updatedPhoto: false,
+            url: "https://quiet-ravine-27369.herokuapp.com"
+        }
+    }
+
+    openWidget = event => {
+        event.preventDefault();
+        const myWidget = window.cloudinary.createUploadWidget({
+            cloudName: "dcokaa0ia",
+            uploadPreset: 'pixelUser'
+        }, (error, result) => {
+            if (result.event === "success") {
+                this.setState({
+                    updatedPhoto: true,
+                    userImage: result.info.url
+                })
+            }
+        });
+        myWidget.open();
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const body = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            bio: this.state.bio,
+            userImage: this.state.userImage,
+        }
+        console.log(body)
+        const url = this.state.url + '/users/' + this.props.author._id
+        fetch(url, {
+            method: 'PUT', 
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(body),
+            })
+            .then((response) => response.json())
+            .then((data) => {
+            console.log('Success:', data);
+                this.props.closeForm()
+                this.props.updateAuthorInfo(body)
+            })
+            .catch((error) => {
+            console.error('Error:', error);
+            });
+        
+
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    render() {
+        const renderImage = (url) => {
+            return <img src={url} alt="" />
+        };
+        return (
+            <React.Fragment>
+                <div onClick={this.props.closeForm} className="closeBar"><i className="far fa-times-circle"></i></div>
+                <form onSubmit={this.handleSubmit}>
+                    <h2>Update Profile</h2>
+                    <div className="splitInputs">
+                        <div className="formGroup">
+                        <label htmlFor="firstName">First Name:</label>
+                        <input
+                            type="text"
+                            value={this.state.firstName}
+                            onChange={this.handleInputChange}
+                            name="firstName"
+                            id="firstName"
+                            placeholder="First Name..."
+                            required
+                        />
+                        </div>
+                        <div className="formGroup">
+                        <label htmlFor="lastName">Last Name:</label>
+                        <input
+                            type="text"
+                            id="lastName"
+                            name="lastName"
+                            value={this.state.lastName}
+                            onChange={this.handleInputChange}
+                            placeholder="Last Name..."
+                            required
+                        />
+                        </div>
+                    </div>
+                        <div className="formGroup">
+                        <label htmlFor="bio">Biography:</label>
+                        <textarea
+                            type="text"
+                            id="bio"
+                            name="bio"
+                            maxLength="100"
+                            value={this.state.bio}
+                            onChange={this.handleInputChange}
+                            className="textarea"
+                            placeholder="Share some info about yourself (100 characters or less)..."
+                        />
+                        </div>
+
+                    <div className="imageSample">
+                        <button onClick={this.openWidget} type="button" className="addPhoto-button">{this.state.uploadImage ? <span>Image Uploaded</span> : "Upload User Image"}</button>
+                        <div className="imageSampleHolder">{this.state.updatedPhoto ? renderImage(this.state.userImage) : renderImage(this.props.author.userImage)}</div>
+                    </div>
+                    <div className="formGroup">
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+            </React.Fragment>
+        )
+    }
+}
+
+export default UpdateProfileForm;
